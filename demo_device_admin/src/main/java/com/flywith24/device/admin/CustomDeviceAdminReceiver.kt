1ecafdaa8edf2120f7a9e0f1a0ffbcc80f778934
Flywith24 <fly_with24@163.com>
2020-09-21 10:21:03 +0800

    add device admin project

---
+++ demo_device_admin/src/main/java/com/flywith24/device/admin/CustomDeviceAdminReceiver.kt
@@ -0,0 +1,24 @@
+package com.flywith24.device.admin
+
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+/**
+ * @author yyz (杨云召)
+ * @date   2020/9/21
+ * time   10:13
+ * description
+ */
+class CustomDeviceAdminReceiver : DeviceAdminReceiver() {
+
+    override fun onEnabled(context: Context, intent: Intent) {
+        Toast.makeText(context, "设备管理员开启", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDisabled(context: Context, intent: Intent) {
+        Toast.makeText(context, "设备管理员关闭", Toast.LENGTH_SHORT).show()
+    }
+}
---
+++ demo_device_admin/src/main/java/com/flywith24/device/admin/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.flywith24.device.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+}
