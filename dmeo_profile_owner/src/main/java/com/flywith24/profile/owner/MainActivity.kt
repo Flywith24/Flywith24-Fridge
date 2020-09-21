c2437fde047044884a8c613b8f42ad46f8a7d7ce
Flywith24 <fly_with24@163.com>
2020-09-21 11:23:31 +0800

    add Profile Owner Project

---
+++ dmeo_profile_owner/src/main/java/com/flywith24/profile/owner/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.flywith24.profile.owner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+}
